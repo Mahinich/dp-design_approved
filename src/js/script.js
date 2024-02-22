2c89d53c4a444e487b76f1e368d4e594f46bfad1
Mahinich <andrey.mahinitch@gmail.com>
2024-02-22 08:19:38 +0200

    adaptive in progress

--- src/js/script.js
+++ src/js/script.js
@@ -1,54 +1,78 @@
 'use strict';
 
-/* FIXED HEADER START */
 window.addEventListener('DOMContentLoaded', () => {
-  window.addEventListener('scroll', function() {
-    let header = document.querySelector('.header');
-    if (window.scrollY > 1) {
-        header.style.cssText = `
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        z-index: 10;
-      `
-    } else {
-        header.style.position = 'relative'; // or your preferred default position
+
+/* SLIDER START */
+
+  const sliderItems = document.querySelectorAll('.game-item');
+  const sliderLine = document.querySelector('.slider-line');
+  const gameItem = document.querySelector('.game-item');
+  const gameItemWidth_margin = gameItem.offsetWidth + parseInt(window.getComputedStyle(gameItem).marginRight);
+  const shiftWidth = gameItemWidth_margin * 3;
+  let count = 0;
+  
+  document.querySelector('.slider .prev').addEventListener('click', function() {
+    count -= 3;
+
+    if (count < 0) {
+      count = sliderItems.length-3;
     }
-  });
-/* FIXED HEADER END */
+    
+    rollSlider();
+    currentNumchange();
+  })
+  
+  document.querySelector('.slider .next').addEventListener('click', function() {
+    count += 3;
 
-/* SLIDRE START */
-  const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+    if (count >= sliderItems.length) {
+      count = 0;
+    }
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'fraction',
-      
-      renderCustom: function (swiper, current, total) {
-        return current + ' of ' + total;
-      },
-     /*  formatFractionCurrent: function(number) {
-        return number;
-      }, */
-    },
+    rollSlider();
+    currentNumchange();
+  })
+  
+  function rollSlider() {
+    sliderLine.style.transform = `translateX(-${count*gameItemWidth_margin}px)`;
+  }
+  
+  // Pagination Start
+  const totalNum = document.querySelector('.slider-order .total-num');
+  const currentNum = document.querySelector('.slider-order .current-num');
 
+  function currentNumchange() {
+    currentNum.textContent = count + 3;
+  }
+  
+  currentNumchange();
+  
+  totalNum.textContent = `${sliderItems.length}`; 
+  // Pagination End
+
+/* SLIDER END */
+
+/* HAMBURGER START*/
+  const hamburger = document.querySelector('.hamburger');
+  const header = document.querySelector('.header');
+  const navList = document.querySelector('.nav-list');
+
+
+  hamburger.addEventListener('click', function() {
+
+    if (navList.classList.contains("visible")) {
+      navList.classList.remove("visible");
+    } else {
+      navList.classList.add("visible");
+    }
     
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+    hamburger.classList.toggle('active')
+    header.classList.toggle('extend');
 
-    slidesPerView: 3,
-    spaceBetween: 61,
+   
+  })
 
-  });
-/* SLIDRE END */
+  
+/* HAMBURGER END*/
 })
-
